ca4ae1ef0fdfbb7d97a43215c5215579fd296dfb
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-05-27 21:04:44 -0300

    CRUD implementado

--- src/main/java/com/example/CRUD/controller/UsersController.java
+++ src/main/java/com/example/CRUD/controller/UsersController.java
@@ -3,11 +3,10 @@ package com.example.CRUD.controller;
 import com.example.CRUD.entities.Users;
 import com.example.CRUD.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UsersController {
@@ -15,10 +14,32 @@ public class UsersController {
     @Autowired
     UsersService usersService;
 
+    @GetMapping
+    @RequestMapping("/findAll")
+    public List<Users> findAll(){
+        return usersService.getUsers();
+    }
+
+    @GetMapping
+    @RequestMapping("/find/{id}")
+    public Optional<Users> findById(@PathVariable Long id){
+        return usersService.getUser(id);
+    }
+
     @PostMapping
     @RequestMapping("/create")
     public Users createUsers(@RequestBody Users users){
         return usersService.createUser(users);
     }
 
+    @PutMapping("/update/{id}")
+    public Users updateUser(@PathVariable Long id, @RequestBody Users updatedUser) {
+        Optional<Users> optionalUser = usersService.updateUser(id, updatedUser);
+        return updatedUser;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteById(@PathVariable Long id){
+        usersService.deleteUser(id);
+    }
 }
--- src/main/java/com/example/CRUD/services/UsersService.java
+++ src/main/java/com/example/CRUD/services/UsersService.java
@@ -5,6 +5,9 @@ import com.example.CRUD.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UsersService {
 
@@ -15,4 +18,23 @@ public class UsersService {
         return usersRepository.save(user);
     }
 
+    public List<Users> getUsers(){
+        return usersRepository.findAll();
+    }
+
+    public Optional<Users> getUser(Long id){
+        return usersRepository.findById(id);
+    }
+
+    public Optional<Users> updateUser(Long id, Users updatedUser) {
+        return usersRepository.findById(id).map(user -> {
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            return usersRepository.save(user);
+        });
+    }
+
+    public void deleteUser(Long id){
+        usersRepository.deleteById(id);
+    }
 }
